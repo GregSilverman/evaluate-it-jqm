ed4591368bfff18ef63f79994a9d06c3e6862a51
gregsilverman <greg.marc.silverman@gmail.com>
2012-09-05 20:27:03 -0500

    added comments; more housecleaning

--- src/org/metroblooms/evaluator/EvaluateItActivity.java
+++ src/org/metroblooms/evaluator/EvaluateItActivity.java
@@ -7,7 +7,8 @@ public class EvaluateItActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	super.setIntegerProperty("loadUrlTimeoutValue", 120000);
+    	/** resolves issue with load js time out: error: "Connection to server was unsuccesful" **/
+	super.setIntegerProperty("loadUrlTimeoutValue", 120000);
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
     }
